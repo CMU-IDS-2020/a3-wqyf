9fa8aacb87f10ce00855a14b6bfc3de4cb8fc086
Weiqin Wang <weiqinw@andrew.cmu.edu>
2020-10-22 23:42:35 -0400

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,10 +52,22 @@ with row01:
 
 st.markdown("### Examining the traffic flow of NYC Yellow Taxi for January 2016")
 
+row1, row2 = st.beta_columns(2)
+with row1:
+    # FILTERING DATA BY HOUR SELECTED
+    start_date = st.date_input('Date', datetime.date(2016, 1, 1))
+    end_date = start_date + datetime.timedelta(days=1)
 
-# FILTERING DATA BY HOUR SELECTED
-start_date = st.date_input('Date', datetime.date(2016, 1, 1))
-end_date = start_date + datetime.timedelta(days=1)
+latitude = ""
+longitude = ""
+with row2:
+    pickdrop = st.radio("Pickup/Dropoff", options=["Pickup", "Dropoff"], index=0)
+    if pickdrop == "Pickup":
+        latitude = 'pickup_latitude'
+        longitude = 'pickup_longitude'
+    else:
+        latitude = 'dropoff_latitude'
+        longitude = 'dropoff_longitude'
 
 @st.cache()
 def load_time(data, start_date, end_date):
@@ -80,9 +92,9 @@ lon = location.longitude
 lat = location.latitude
 
 @st.cache()
-def load_geo(data, lon, lat, radius):
+def load_geo(data, lon, lat, radius, latitude, longitude):
     data['dist'] = list(
-        map(lambda k: get_distance(data.loc[k]['pickup_longitude'], data.loc[k]['pickup_latitude'], lon, lat),
+        map(lambda k: get_distance(data.loc[k][longitude], data.loc[k][latitude], lon, lat),
             data.index))
     # default is radius within 1 mile
     hold = data[data['dist'] < radius]
@@ -93,7 +105,7 @@ def load_geo(data, lon, lat, radius):
         data = hold
     return data
 
-data = load_geo(data, lon, lat, radius)
+data = load_geo(data, lon, lat, radius, latitude, longitude)
 data = data.copy()
 
 # get the velocity
@@ -106,7 +118,7 @@ data['velo'] = list(map(lambda k: get_velocity(data.loc[k]['pickup_longitude'], 
                                                data.loc[k]['dropoff_longitude'], data.loc[k]['dropoff_latitude'],
                                                data.loc[k][DATE_TIME], data.loc[k]["dropoff_datetime"]), data.index))
 
-st.write("Select time range of pickup")
+st.write("Select time range")
 row21,_, row22,_ = st.beta_columns([9,1,9,1])
 with row21:
     start_hour = st.slider("From:", 0, 23)
@@ -117,11 +129,11 @@ if start_hour>end_hour: st.error("start hour need to be less than or equal to en
 
 hist = np.histogram(data[DATE_TIME].dt.hour, bins=25, range=(0, 25))[0]
 
-chart_data = pd.DataFrame({"hour": range(25), "pickups": hist})
+chart_data = pd.DataFrame({"hour": range(25), "count": hist})
 chart_data['start'] = start_hour
 chart_data['end'] = end_hour
 chart1 = alt.Chart(chart_data).mark_area(interpolate='step-after',)\
-    .encode(x=alt.X("hour:O", scale=alt.Scale(nice=False)),y=alt.Y("pickups:Q"),tooltip=['hour', 'pickups'])\
+    .encode(x=alt.X("hour:O", scale=alt.Scale(nice=False)),y=alt.Y("count:Q"),tooltip=['hour', 'count'])\
     .configure_mark(opacity=0.5,color='red')
 # chart2 = alt.Chart(chart_data).mark_rule().encode(x='start')
 # c = alt.Chart(chart1 + chart2)
@@ -140,32 +152,33 @@ st.markdown("### Current period and location has " + str(data.shape[0]) + " reco
 hour_text = st.empty()
 map = st.empty()
 st.sidebar.markdown('### Map Options')
-layer_names = [layer_name for layer_name in ["Hexagon", "Heatmap", "Path"] if st.sidebar.checkbox(layer_name, False)]
+layer_names = [layer_name for layer_name in ["Scatter", "Heatmap", "Path"] if st.sidebar.checkbox(layer_name, False)]
 
 st.sidebar.markdown("**In Path Map, a more efficient ride is green while a less efficient ride is red**")
 st.sidebar.markdown("efficiency = great circle distance between pickup and dropoff / trip duration")
 
-st.sidebar.markdown("### Animation")
-animations = {"None": None, "Slow": 3, "Fast": 1}
-animate = st.sidebar.radio("Animation Choices", options=list(animations.keys()), index=0)
+st.sidebar.markdown("### Animation Choices")
+animations = {"None": None, "Non-cumulative": 1.5, "Cumulative": 1.5}
+animate = st.sidebar.radio("For better experience, change back to 'none' animation before other operations", options=list(animations.keys()), index=0)
 animation_speed = animations[animate]
 
-def fmap(data, lat, lon, zoom, layer_names):
+def fmap(data, lat, lon, zoom, layer_names, latitude, longitude):
     all_layers = {
-        "Hexagon": pdk.Layer(
-            "HexagonLayer",
+
+       "Scatter": pdk.Layer(
+            'ScatterplotLayer',
             data=data,
-            get_position=["pickup_longitude", "pickup_latitude"],
-            radius=100,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            extruded=True,
-        ),
+            get_position=[longitude, latitude],
+            auto_highlight=True,
+            get_radius=50,
+            get_fill_color='[180, 0, 200, 1000]',
+            pickable=True), 
+
         "Heatmap": pdk.Layer(
             "HeatmapLayer",
             data=data,
             opacity=0.9,
-            get_position=["pickup_longitude", "pickup_latitude"],
+            get_position=[longitude, latitude],
             aggregation='"SUM"',
         ),
 
@@ -215,20 +228,26 @@ else:
         zoom_level = 11
     else:
         zoom_level = 10
-midpoint = (np.average(data["pickup_latitude"]), np.average(data["pickup_longitude"]))
+midpoint = (np.average(data[latitude]), np.average(data[longitude]))
 
-if animation_speed:
+if animate == "Non-cumulative":
     for i in cycle(timewindow):
         start_time = datetime.datetime(2016,1,1,start_hour,0,0) + i * timestep
         end_time = datetime.datetime(2016,1,1,start_hour,0,0) + (i+1) * timestep
         hour_text.write("**Between " + str(start_time.time()) + " and " + str(end_time.time()) + "**")
         selected_data = data[(data[DATE_TIME].dt.time >= start_time.time()) & (data[DATE_TIME].dt.time < end_time.time())]
-
-
-        fmap(selected_data, midpoint[0], midpoint[1], zoom_level, layer_names)
+        fmap(selected_data, midpoint[0], midpoint[1], zoom_level, layer_names, latitude, longitude)
+        time.sleep(animation_speed)
+elif animate == "Cumulative":
+    for i in cycle(timewindow):
+        start_time = datetime.datetime(2016,1,1,start_hour,0,0)
+        end_time = datetime.datetime(2016,1,1,start_hour,0,0) + (i+1) * timestep
+        hour_text.write("**Between " + str(start_time.time()) + " and " + str(end_time.time()) + "**")
+        selected_data = data[(data[DATE_TIME].dt.time >= start_time.time()) & (data[DATE_TIME].dt.time < end_time.time())]
+        fmap(selected_data, midpoint[0], midpoint[1], zoom_level, layer_names, latitude, longitude)
         time.sleep(animation_speed)
 else:
     start_time = datetime.datetime(2016, 1, 1, start_hour, 0, 0)
     end_time = datetime.datetime(2016, 1, 1, end_hour, 59, 59)
     hour_text.write("**Between " + str(start_time.time()) + " and " + str(end_time.time()) + "**")
-    fmap(data, midpoint[0], midpoint[1], zoom_level, layer_names)
+    fmap(data, midpoint[0], midpoint[1], zoom_level, layer_names, latitude, longitude)
